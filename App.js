2c243ea572d9335df60051733782013b86270143
IttaRodrigues <itamararodriguesdeoliveira@gmail.com>
2024-10-31 22:24:42 -0300

    ajusta os botões

---
+++ App.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Botao from './components/Botao';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.botoes}>
+
+        <Botao label="AC" triplo/>
+        <Botao label="/" operacao/>
+
+        <Botao label="7"/>
+        <Botao label="8"/>
+        <Botao label="9"/>
+        <Botao label="*" operacao/>
+
+        <Botao label="4"/>
+        <Botao label="5"/>
+        <Botao label="6"/>
+        <Botao label="-" operacao/>
+
+        <Botao label="1"/>
+        <Botao label="2"/>
+        <Botao label="3"/>
+        <Botao label="+" operacao/>
+
+        <Botao label="0" duplo/>
+        <Botao label="."/>
+        <Botao label="=" operacao/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  botoes: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+});
